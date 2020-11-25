4bfa01fe96f3d53d32b8ef51a59badda658a92e5
Pedro Macias <pin.macias@gmail.com>
2020-11-25 20:00:03 +0100

    inicio aplicacion

---
+++ app.py
@@ -0,0 +1,50 @@
+# MINUTO 23.08
+
+
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+# conectarse a la base de datos
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'pedro'
+app.config['MYSQL_PASSWORD'] = '123'
+app.config['MYSQL_HOS'] = 'contactos_flask'
+
+mysql = MySQL(app)
+
+# rutas de las paginas
+@app.route('/')
+def index ():
+    return render_template('index.html')
+
+@app.route('/agregar_contactos', methods = ['POST'])
+def agregar_contactos ():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        telefono = request.form['telefono']
+        email = request.form['email']
+        print(nombre)
+        print(telefono)
+        print(email)
+        return 'recibido'
+
+
+@app.route('/editar')
+def editar():
+    return 'editar contactos'
+
+@app.route('/borrar')
+def borrar():
+    return ' borrar contactos'
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(port = 3000, debug = True)
+    # http://localhost:3000
